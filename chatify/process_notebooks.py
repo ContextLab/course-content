b522d8e455b40a6ffdb019904b1babdf711e488d
jeremymanning <jeremy.r.manning@dartmouth.edu>
2023-07-08 14:02:16 -0400

    modify "source" tutorial notebooks

--- chatify/process_notebooks.py
+++ chatify/process_notebooks.py
@@ -10,8 +10,7 @@ mod_repo = 'ContextLab'
 
 
 def get_tutorial_notebooks(basedir):
-    return lsdir(os.path.join(basedir, 'tutorials', 'W*D*_*', 'student', 'W*D*_Tutorial*.ipynb')) + \
-           lsdir(os.path.join(basedir, 'tutorials', 'W*D*_*', 'instructor', 'W*D*_Tutorial*.ipynb'))
+    return lsdir(os.path.join(basedir, 'tutorials', 'W*D*_*', 'W*D*_Tutorial*.ipynb'))
 
 
 def chatified(fname):
