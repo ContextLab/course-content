78dc297f686f6480421ff378d951cad440066142
jeremymanning <jeremy.r.manning@dartmouth.edu>
2023-07-08 13:40:57 -0400

    "chatify" tutorial notebooks

---
+++ chatify/process_notebooks.py
@@ -0,0 +1,88 @@
+import os
+from glob import glob as lsdir
+
+import nbformat as nbf
+from chatify import Chatify
+from tqdm import tqdm
+
+source_repo = 'NeuromatchAcademy'
+mod_repo = 'ContextLab'
+
+
+def get_tutorial_notebooks(basedir):
+    return lsdir(os.path.join(basedir, 'tutorials', 'W*D*_*', 'student', 'W*D*_Tutorial*.ipynb')) + \
+           lsdir(os.path.join(basedir, 'tutorials', 'W*D*_*', 'instructor', 'W*D*_Tutorial*.ipynb'))
+
+
+def chatified(fname):
+    notebook = nbf.read(fname, nbf.NO_CONVERT)
+    header_cell = notebook['cells'][0]
+    return mod_repo in header_cell['source']
+
+
+def get_text(fname):
+    with open(os.path.join(os.getcwd(), 'chatify', fname), 'r') as f:
+        return ''.join(f.readlines())
+
+
+def inject_chatify(fname):
+    notebook = nbf.read(fname, nbf.NO_CONVERT)
+    new_notebook = notebook.copy()
+
+    # update header cell
+    header_cell = new_notebook['cells'][0]
+    if 'id' in header_cell['metadata']:
+        del header_cell['metadata']['id']
+    header_cell['source'] = header_cell['source'].replace(source_repo, mod_repo)
+
+    # insert background cell
+    background_cell = nbf.v4.new_markdown_cell(source=get_text('background.md'), metadata={'execution': {}})
+    del background_cell['id']
+
+    # insert davos cell
+    davos_cell = nbf.v4.new_code_cell(source=get_text('install_davos.py'), metadata={'cellView': 'form', 'execution': {}})
+    del davos_cell['id']
+
+    # insert chatify cell
+    chatify_cell = nbf.v4.new_code_cell(source=get_text('install_and_load_chatify.py'), metadata={'cellView': 'form', 'execution': {}})
+    del chatify_cell['id']
+
+    if chatified(fname):
+        new_notebook.cells[0] = header_cell
+        new_notebook.cells[1] = background_cell
+        new_notebook.cells[2] = davos_cell
+        new_notebook.cells[3] = chatify_cell
+    else:
+        new_notebook.cells.insert(1, background_cell)
+        new_notebook.cells.insert(2, davos_cell)
+        new_notebook.cells.insert(3, chatify_cell)
+
+    # Write the file
+    nbf.write(
+        new_notebook,
+        fname,
+        version=nbf.NO_CONVERT,
+    )
+
+
+def cache_responses(fname, tutor=None, prompts=None):
+    if tutor is None:
+        tutor = Chatify()
+    
+    if prompts is None:
+        prompts = tutor._read_prompt_dir()['tutor']
+    
+    notebook = nbf.read(fname, nbf.NO_CONVERT)
+    for cell in notebook['cells']:
+        if cell['cell_type'] == 'code':
+            for _, prompt in prompts.items():
+                tutor._cache(cell['source'], prompt)
+
+
+tutorials = get_tutorial_notebooks(os.getcwd())
+tutor = Chatify()
+prompts = tutor._read_prompt_dir()['tutor']
+
+for notebook in tqdm(tutorials):
+    inject_chatify(notebook)
+    cache_responses(notebook, tutor=tutor, prompts=prompts)
